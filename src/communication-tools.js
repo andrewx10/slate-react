829638ab652a1e5669feb7b8ff28c3cc4d57bd79
Andrew Shaw <ashaw85@hotmail.com>
2019-10-28 14:52:17 -0700

    slate front end

---
+++ src/communication-tools.js
@@ -0,0 +1,112 @@
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+import UserAndId from './billing'
+import Slate from './slate'
+
+'use strict';
+
+class CommunicationTools extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            message: '',
+            loading: false,
+            messageMaxLength: 3,
+            billFieldLoadingMessage: 'Loading...'
+        };
+    }
+    
+    static getDerivedStateFromError(error) {
+      console.log('getDerivedState');
+      return { hasError: true };
+    }
+
+    componentDidCatch(error, errorInfo) {
+      // You can also log the error to an error reporting service
+      //logErrorToMyService(error, errorInfo);
+    }
+    
+    componentDidMount() {
+      async function main() {
+        let response;
+        this.setState({
+          loading: true
+        })
+        if (location.href.match(/slate\/(.*)\/(.*)/) !== null) {
+          response = await fetch('/slate/get/'+location.href.match(/slate\/(.*)\/(.*)/)[1] + 
+              '/' + location.href.match(/slate\/(.*)\/(.*)/)[2]);
+        }
+        else {
+          response = await fetch('../../slate/get/'+location.href.match(/slate\/(.*)/)[1]);
+
+        }
+        const getData = await response.json();
+        if (getData.length > 0) {
+          this.setState({
+            loading: false,
+            message: getData[0].message,
+            messageMaxLength: getData[0].key == '123' ? 3 : 300,
+            readStatus: getData[0].viewedTime > getData[0].updateTime ? 
+                'Read by recipient.' : 'Not read by recipient.'
+          });
+        }
+        if (this.state.loading) {
+          ReactDOM.render(<div>Loading...</div>, document.getElementById('slateForm'));
+        }
+        else {
+          ReactDOM.render(<Slate message={this.state.message} messageMaxLength={this.state.messageMaxLength} readStatus={this.state.readStatus} callback={this.formChild1.bind(this)} />, document.getElementById('slateForm'));
+        }
+      }
+      main = main.bind(this);
+      main();
+      
+      async function handleClick(e) {
+        e.preventDefault(0);
+      }
+      async function postData(url = '', data = {}) {
+        const response = await fetch(url, {
+          method: 'POST',
+          mode: 'cors',
+          cache: 'no-cache',
+          credentials: 'same-origin',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          redirect: 'follow',
+          referrer: 'no-referrer',
+          body: JSON.stringify(data)
+        });
+        return await response.json();
+      }
+    }
+    
+    componentDidUpdate() {
+      ReactDOM.render(<UserAndId user="Joan" userId="134" data={this.state.data}/>, document.getElementById('intro'))
+    }
+    
+    formChild1(params) {
+      this.setState({
+        data: params
+      })
+    }
+
+    render() {
+        if (this.state.hasError) {
+          return <h1>Something went wrong.</h1>;
+        }
+//        return React.createElement(
+//            'ul',
+//            { 
+//                className: "flex-container",
+//                dangerouslySetInnerHTML: {__html: this.state.billFieldLoadingMessage } 
+//            }
+//        );
+        return <div id="communicationTools">
+        </div>;
+    }
+}
+//ReactDOM.render(React.createElement(CommunicationTools),
+//    document.querySelector('#communication-tools-container'));
+ReactDOM.render(<CommunicationTools/>,
+    document.querySelector('#communication-tools-container'));
---
+++ src/slate.js
@@ -0,0 +1,95 @@
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
+import RegisterMe from './register-me'
+
+'use strict';
+
+export default class Slate extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+        message2: 'x',
+        message: this.props.message,
+        messageSubmitButtonText: 'Submit message',
+        hasError: false
+    };
+    this.textAreaRef = React.createRef();
+    this.handleClick = this.handleClick.bind(this);
+  }
+  
+  static getDerivedStateFromError(error) {
+    return { hasError: true };
+  }
+  
+  getContent(event) {
+    this.props.callback(event.target.value);
+  }
+  
+  handleClick(e) {
+    e.preventDefault(0);
+    
+    let postData = async function (url = '', data = {}) {
+      this.setState({ messageSubmitButtonText: 'Submitting'});
+      const firstData = await fetch(url, {
+        method: 'POST',
+        mode: 'cors',
+        cache: 'no-cache',
+        credentials: 'same-origin',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        redirect: 'follow',
+        referrer: 'no-referrer',
+        body: JSON.stringify(data)
+      });
+      let response = await firstData.json();
+      if(response.status === 'processed') {
+        this.setState({ messageSubmitButtonText: 'Submitted'});
+        setTimeout(()=>{
+          this.setState({ messageSubmitButtonText: 'Submit message'});
+        }, 3000);
+      } 
+      return;
+    }
+    postData = postData.bind(this);
+    try {
+      async function updateMessage() {
+        const data = await postData('/slate/post/'+location.href.match(/slate\/(.*)/)[1],
+          { message: document.getElementById('messageTextArea').value } );
+      }
+      updateMessage();
+    }
+    catch (error) {
+      console.log(error);
+    }
+  }
+  
+  componentDidMount() {
+    this.textAreaRef.current.focus();
+    this.setState({
+      message: this.props.message
+    })
+  }
+  
+  render() {
+    if (this.state.hasError) {
+      return <h1>Something went wrong.</h1>;
+    }
+    let messageForm = <form id="messageForm">
+    <div><label>Message:</label></div>
+    <textarea id="messageTextArea" rows="10" cols="50" maxLength={this.props.messageMaxLength} ref={this.textAreaRef} defaultValue={this.props.message} onChange={this.onChange}></textarea>
+    <p id="readStatus">{this.props.readStatus}</p>
+    <input type="submit" onClick={this.handleClick} onChange={this.getContent.bind(this)}/>
+  </form>;
+    return <div id="slate">
+      <form id="messageForm">
+        <div><label>Message:</label></div>
+        <textarea id="messageTextArea" rows="10" cols="50" maxLength={this.props.messageMaxLength} ref={this.textAreaRef} defaultValue={this.props.message} onChange={this.onChange}></textarea>
+        <p id="readStatus">{this.props.readStatus}</p>
+        <input type="submit" value={this.state.messageSubmitButtonText} onClick={this.handleClick} onChange={this.getContent.bind(this)}/>
+      </form>
+        {location.href.match(/slate\/(.*?)\//)[1] === '123' ? <RegisterMe /> : ''}
+        
+    </div>
+  }
+}
